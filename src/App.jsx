e18f49c4f2d4d180345b9109b37016590127e48a
shivam sharma <shivamsharma38391@gmail.com>
2023-10-07 13:15:35 +0530

    testing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Card from "./components/card";
 
-import Button from "./components/button";
+import "./App.css";
+// import Card from "./components/card";
+
+// import Button from "./components/button";
 import button from "./components/button";
 
 function App() {
